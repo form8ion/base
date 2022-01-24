df0fa98eda5378051075f22207b0fc582f5ce45a
Matt Travi <programmer@travi.org>
2022-01-23 23:21:27 -0600

    feat(settings): defined default content for the settings config file

--- src/settings/scaffolder-test.js
+++ src/settings/scaffolder-test.js
@@ -22,7 +22,32 @@ suite('repository settings', () => {
   test('that the account-level base config for the settings app is defined', async () => {
     const pathToGithubDirectory = any.string();
     const dumpedYaml = any.string();
-    jsYaml.dump.withArgs({}).returns(dumpedYaml);
+    jsYaml.dump
+      .withArgs({
+        repository: {
+          has_wiki: false,
+          has_projects: false,
+          has_downloads: false,
+          allow_squash_merge: false,
+          allow_merge_commit: true,
+          allow_rebase_merge: true,
+          delete_branch_on_merge: true
+        },
+        branches: [{
+          name: 'master',
+          protection: {
+            required_pull_request_reviews: null,
+            required_status_checks: null,
+            restrictions: null,
+            enforce_admins: true
+          }
+        }],
+        teams: [{
+          name: 'maintainers',
+          permission: 'push'
+        }]
+      })
+      .returns(dumpedYaml);
 
     await scaffoldSettingsConfig({pathToGithubDirectory});
 
